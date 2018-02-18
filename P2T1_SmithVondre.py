6e9249da9258f2329988808792c3d64e2742b653
VSmith21 <36609351+VSmith21@users.noreply.github.com>
2018-02-18 17:16:20 -0500

    Add files via upload
    First submission

---
+++ P2T1_SmithVondre.py
@@ -0,0 +1,17 @@
+#CTI-110
+#P2T1 -Sales Prediction
+#Vondre Smith
+#2.13.18
+#
+
+#Get the projected total sales.
+
+total_sales=float(input("Enter the projected sales: "))
+
+#Calculate the profit as 23 percent of total sales.
+
+profit= total_sales * .23
+
+#Display the profit.
+
+print ("The profit is $", profit)
