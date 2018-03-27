90b17c81b76e6289dca0f5c95561e91c435c8c6e
VSmith21 <36609351+VSmith21@users.noreply.github.com>
2018-03-27 17:37:03 -0400

    Add files via upload

---
+++ P4HW3_VondreSmith.py
@@ -0,0 +1,14 @@
+#Vondre Smith
+#CTI - 110
+#P4HW3 - Factorial
+#03.27.2018
+
+n = int(input("Enter a nonnegative integer: "))
+num = 1
+
+for i in range(1,n+1):
+    num = num*i
+
+print("The factorial is: ", num)
+
+
